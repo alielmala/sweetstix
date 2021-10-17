8a463ff0154fc00f2efdcf955a1e915f5f8803a8
alielmala <ali.elmala22@gmail.com>
2021-10-17 17:46:09 +0200

    blog page

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+var sideBar = document.getElementById("side-responsive");
+
+function sideBarController(){
+    sideBar.classList.add("offcanvas");
+    sideBar.classList.add("offcanvas-start")
+    document.getElementById("side-con").classList.add("offcanvas-body");
+}
